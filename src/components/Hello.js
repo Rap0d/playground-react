95f1c904a72408e93dc2835c832537ac409f155b
Rap0d <40348849+Rap0d@users.noreply.github.com>
2021-06-14 14:06:48 +0900

    JSX 예제 작성 및 SCSS 모듈 추가

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,20 @@
 import React from "react";
+import '../scss/Hello.scss'
 
 function Hello() {
-    return <div>Hello!</div>
+    const name = 'React';
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24,
+        padding: '1rem'
+    }
+    return (
+        <>
+            <div style={style}>Hello! {name}</div>
+            <div className="gray-box">gray</div>
+        </>
+    )
 }
 
 export default Hello;
