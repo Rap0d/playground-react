62b9dade92ba8f0be92c2609456b3284d2e28593
Rap0d <40348849+Rap0d@users.noreply.github.com>
2021-06-15 09:01:34 +0900

    useRef로 특정 DOM 선택하기

--- src/components/tutorials/InputSample.js
+++ src/components/tutorials/InputSample.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 
 function InputSample() {
     const [inputs, setInputs] = useState({
@@ -6,15 +6,16 @@ function InputSample() {
         nId: ''
     })
 
-    const {name, nId} = inputs
+    // useRef로 특정 DOM 선택(초기화 하지 않으면 warning 뜸)
+    const nameInput = useRef(null)
+
+    const {name, nId} = inputs          // 비구조화 할당을 통해 값 추출
 
     const onChange = (e) => {
-        const {value, name} = e.target
-        console.log(value)
-        console.log(name)
+        const {value, name} = e.target  // e.target에서 name, value 추출
         setInputs({
-            ...inputs,
-            [name]: value
+            ...inputs,                  // 기존의 input 객체 복사
+            [name]: value               // name 키를 가진 값을 value로 설정
         })
     }
 
@@ -24,20 +25,27 @@ function InputSample() {
             name: '',
             nId: ''
         })
+
+        // 초기화 버튼을 클릭했을 때 name input에 포커스가 잡히도록 구현
+        nameInput.current.focus()
     }
 
     return (
         <>
+            {/* ref를 선택해주어야 함 */}
             <input
                 placeholder={'Name'}
                 name={'name'}
                 onChange={onChange}
-                value={name}/>
+                value={name}
+                ref={nameInput}
+            />
             <input
                 placeholder={'nId'}
                 name={'nId'}
                 onChange={onChange}
-                value={nId}/>
+                value={nId}
+            />
             <button onClick={onReset}>Reset</button>
             <div>
                 <b>Value: </b>
