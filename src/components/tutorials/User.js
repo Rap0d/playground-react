2041a2e4cd7955d7cfb69c5b8b52fd4c8682f0aa
Rap0d <40348849+Rap0d@users.noreply.github.com>
2021-06-24 13:48:11 +0900

    react-async 로 요청 상태 관리

--- src/components/tutorials/User.js
+++ src/components/tutorials/User.js
@@ -1,7 +1,12 @@
 import axios from "axios";
-import useAsync from "./useAsync";
+import {useAsync} from "react-async";
 
-async function getUser(id) {
+// import useAsync from "./useAsync";
+
+/*
+useAsync를 사용할 때는 프로미스를 반환하는 함수의 파라미터를 객체형태로 넣음
+ */
+async function getUser({id}) {
     const response = await axios.get(
         `https://jsonplaceholder.typicode.com/users/${id}`
     )
@@ -12,10 +17,18 @@ function User({id}) {
     /*
     id가 바뀔때마다 재호출되도록 deps에 id 넣어줌
      */
-    const [state] = useAsync(() => getUser(id), [id])
-    const {loading, data: user, error} = state
+    // const [state] = useAsync(() => getUser(id), [id])
+    // const {loading, data: user, error} = state
 
-    if (loading) {
+    // react-async를 사용한 async
+    // watch 값에 특정 값을 넣어주면 이 값이 바뀔때 마다 promiseFn에 넣은 함수 다시 호출
+    const {data: user, error, isLoading} = useAsync({
+        promiseFn: getUser,
+        id,
+        watch: id
+    })
+
+    if (isLoading) {
         return <div>로딩중..</div>;
     }
     if (error) {
--- src/components/tutorials/UsersWithHook.js
+++ src/components/tutorials/UsersWithHook.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+// import useAsync from "./useAsync";
 import User from "./User";
+import {useAsync} from "react-async";
 
 /* useAsync에서는 Promise의 결과를 바로 data에 담기 때문에
  요청을 한 이후 response에서 data 를 추출하여 반환
@@ -17,13 +18,24 @@ function UsersWithHook() {
     // useState를 사용하여 userId 상태 관리
     // 리스트에 있는 항목을 클릭하면 사용자의 id를 userId로 설정
     const [userId, setUserId] = useState(null)
-    const [state, refetch] = useAsync(getUsers, [], true)
+    // const [state, refetch] = useAsync(getUsers, [], true)
+    //
+    // // state.data를 users 키워드로 조회
+    // const {loading, data: users, error} = state
 
-    // state.data를 users 키워드로 조회
-    const {loading, data: users, error} = state
+    /*
+    react-async 로 변경
+    만약 useAsync 커스텀 훅에서 사용한 skip과 같이
+    사용자의 특정 인터렉션에 따라 API를 호출하고 싶을때는
+    promiseFn이 아닌 deferFn을 사용하고,
+    reaload 대신 run 함수를 사용한다.
+     */
+    const {data: users, error, isLoading, reload} = useAsync({
+        promiseFn: getUsers
+    });
 
     // 로딩중일때 `로딩중..` 텍스트 표시
-    if (loading) {
+    if (isLoading) {
         return <div>로딩중..</div>;
     }
     // 에러가 발생했을 때 에러 메시지 표시
@@ -32,7 +44,7 @@ function UsersWithHook() {
     }
     // users가 없으면 아무것도 보여주지 않음
     if (!users) {
-        return <button onClick={refetch}>Load</button>;
+        return <button onClick={reload}>Load</button>;
     }
 
     return (
@@ -48,7 +60,7 @@ function UsersWithHook() {
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>Reload</button>
+            <button onClick={reload}>Reload</button>
             {userId && <User id={userId}/>}
         </>
     )
