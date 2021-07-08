c9248851a9e8275408848b530a51cf90cbc316cd
Rap0d <40348849+Rap0d@users.noreply.github.com>
2021-07-08 11:32:02 +0900

    redux 적용

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import {createStore} from 'redux'
+import {Provider} from "react-redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer)
+console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <Provider store={store}>
+                <App/>
+            </Provider>,
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
---
+++ src/modules/counter.js
@@ -0,0 +1,49 @@
+/*
+Action Type
+Ducks 패턴을 따를 때 액션 이름에 접두사를 넣는다.
+-> 다른 모듈간의 액션 이름이 중복되는 것을 방지할 수 있음
+ */
+
+const SET_DIFF = 'counter/SET_DIFF'
+const INCREASE = 'counter/INCREASE'
+const DECREASE = 'counter/DECREASE'
+
+/*
+Action 생성 함수
+ */
+export const setDiff = diff => ({type: SET_DIFF, diff})
+export const increase = () => ({type: INCREASE})
+export const decrease = () => ({type: DECREASE})
+
+/*
+초기 상태 선언
+ */
+const initialState = {
+    number: 0,
+    diff: 1
+}
+
+/*
+Reducer 선언
+ */
+export default function counter(state = initialState, action) {
+    switch (action.type) {
+        case SET_DIFF:
+            return {
+                ...state,
+                diff: action.diff
+            }
+        case INCREASE:
+            return {
+                ...state,
+                number: state.number + state.diff
+            }
+        case DECREASE:
+            return {
+                ...state,
+                number: state.number - state.diff
+            }
+        default:
+            return state
+    }
+}
