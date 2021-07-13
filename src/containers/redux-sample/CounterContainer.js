47577ddc3dc6690e1aa94141b68ebc18fe6cf01b
Rap0d <40348849+Rap0d@users.noreply.github.com>
2021-07-13 16:56:52 +0900

    useSelector 최적화

--- src/containers/redux-sample/CounterContainer.js
+++ src/containers/redux-sample/CounterContainer.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import {useDispatch, useSelector} from "react-redux";
+import {shallowEqual, useDispatch, useSelector} from "react-redux";
 import {decrease, increase, setDiff} from "../../modules/counter";
 import Counter from "../../components/redux-sample/Counter";
 
 function CounterContainer() {
     /*
     useSelector: redux store의 상태를 조회하는 Hook
-    state의 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일
+    state의 값은 `store.getState()` 함수를 호출했을 때 나타나는 결과물과 동일
+
+    useSelector의 두 번째 파라미터는 equalityFn
+    -> `equalityFn?: (left: any, right: any) => boolean`
+    이전 값과 다음 값을 비교하여 true가 나오면 리렌더링을 하지 않고
+    false가 나오면 리렌더링을 한다
+    `shallowEqual`은 `react-redux` 내장함수로, 객체 안의 가장 겉 값들을 모두 비교
      */
     const {number, diff} = useSelector(state => ({
-        number: state.counter.number,
-        diff: state.counter.diff
-    }))
+            number: state.counter.number,
+            diff: state.counter.diff
+        }), shallowEqual
+    )
 
     const dispatch = useDispatch()
 
